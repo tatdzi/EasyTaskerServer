4be9cda0c8cdd7d70de83fcfd5db745f80181ead
Dzianis Tatur <tatdzi@tatdzi.local>
2023-07-27 17:47:36 +0300

    add feign client for audit and changed nginx config for audit-service

---
+++ user-service/src/main/java/by/taskManager/user_service/endpoints/web/controllers/IFeignClientAudit.java
@@ -0,0 +1,13 @@
+package by.taskManager.user_service.endpoints.web.controllers;
+
+import by.taskManager.user_service.core.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(url = "http://audit-service:8080")
+public interface IFeignClientAudit {
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    void saveItem(UserDTO dto,String text);
+
+}
