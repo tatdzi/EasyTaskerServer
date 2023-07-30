970f01d004a0c2b1009b0b51e271b21c948cd680
Dzianis Tatur <tatdzi@tatdzi.local>
2023-07-30 17:14:44 +0300

    changed tokenDTO

--- utils/src/main/java/by/TaskManeger/utils/dto/TokenDTO.java
+++ utils/src/main/java/by/TaskManeger/utils/dto/TokenDTO.java
@@ -2,20 +2,32 @@ package by.TaskManeger.utils.dto;
 
 
 public class TokenDTO {
+    private String uuid;
     private String mail;
-    private String role;
+    private String fio;
+    private UserRole role;
 
-    public TokenDTO(String mail, String role) {
+
+    public TokenDTO(String uuid, String mail, String fio, UserRole role) {
+        this.uuid = uuid;
         this.mail = mail;
+        this.fio = fio;
         this.role = role;
     }
 
+    public String getUuid() {
+        return uuid;
+    }
 
     public String getMail() {
         return mail;
     }
 
-    public String getRole() {
+    public String getFio() {
+        return fio;
+    }
+
+    public UserRole getRole() {
         return role;
     }
 }
