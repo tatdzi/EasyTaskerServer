a11483626aad3a282f3071331f832662843b73fb
Dzianis Tatur <tatdzi@tatdzi.local>
2023-08-13 18:45:03 +0300

    added test config file

--- audit-service/src/test/java/by/taskManager/auditservice/AuditServiceApplicationTests.java
+++ audit-service/src/test/java/by/taskManager/auditservice/AuditServiceApplicationTests.java
@@ -2,6 +2,7 @@ package by.taskManager.auditservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class AuditServiceApplicationTests {
