f2b7d7563548373abaa7e1e3e20f9c8e8a2c3306
Dzianis Tatur <tatdzi@tatdzi.local>
2023-08-14 09:20:29 +0300

    added test.yml, caneged feign clients

--- task-service/src/main/java/by/taskManager/taskservice/endpoint/service/controller/IFeignClientAudit.java
+++ task-service/src/main/java/by/taskManager/taskservice/endpoint/service/controller/IFeignClientAudit.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "${audit-service.name}",url = "${audit-service.url}")
+@FeignClient(name = "audit-service")
 public interface IFeignClientAudit {
     @RequestMapping(value = "/service/save",method = RequestMethod.POST)
     HttpStatus saveItem(AuditDTO audit);
